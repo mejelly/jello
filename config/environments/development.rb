ab395e04954e1394e947ff1442b2716d16ebeaff
mrteera <inbox@mrteera.com>
2016-11-13 17:45:07 +0700

    Fix acceptance tests #14

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.web_console.whitelisted_ips = '172.19.0.0/16'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
