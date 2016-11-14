a87a3f84d0c04bdcc5f884a8be480f27639b9130
mrteera <inbox@mrteera.com>
2016-11-15 01:24:09 +0700

    Fix SSH configuration and its directory #14

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mv ../.travis/deploy_key ~/.ssh/id_rsa
+cp .travis/proxy ~/.ssh/config
+mv .travis/deploy_key ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 sed -i "s#image: {}#image: $REPO:$COMMIT#g" docker-compose.production.yml
 scp docker-compose.production.yml $STAGING_USERNAME:/home/mejelly/jello
