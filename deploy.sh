2e4d528b60e7e0063a60ba84e5947184cf545b88
mrteera <inbox@mrteera.com>
2016-11-15 01:36:35 +0700

    Fix deploy script #14

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp .travis/proxy ~/.ssh/config
+cp .travis/ssh_config ~/.ssh/config
 mv .travis/deploy_key ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 sed -i "s#image: {}#image: $REPO:$COMMIT#g" docker-compose.production.yml
-scp docker-compose.production.yml $STAGING_USERNAME:/home/mejelly/jello
+scp docker-compose.production.yml $STAGING_USERNAME@$STAGING_HOSTNAME:/home/mejelly/jello
 pip install fabric
 fab staging deploy
