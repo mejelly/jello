82d185bdf623d9d8a9024949ed023d8005839dd8
mrteera <inbox@mrteera.com>
2016-12-05 08:05:04 +0700

    Add test for purge cache [skip ci]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'vcr'
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
-  config.hook_into :webmock, :faraday
+  config.hook_into :webmock, :faraday, :typhoeus
   config.configure_rspec_metadata!
   # config.allow_http_connections_when_no_cassette = true
 end
