de51885a644fd0b52747668c51ac9b154e005924
mrteera <inbox@mrteera.com>
2016-12-06 02:42:43 +0700

    Remove auth0 logout mechanism, just simple logout :(

--- app/helpers/logout_helper.rb
+++ app/helpers/logout_helper.rb
@@ -1,11 +1,2 @@
 module LogoutHelper
-  def logout_url
-    creds = { client_id: Rails.application.secrets.auth0_client_id,
-              client_secret: Rails.application.secrets.auth0_client_secret,
-              api_version: 1,
-              domain: Rails.application.secrets.auth0_domain }
-    client = Auth0Client.new(creds)
-    # client.logout_url(root_url)
-    root_url
-  end
 end
--- spec/controllers/logout_controller_spec.rb
+++ spec/controllers/logout_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe LogoutController, type: :controller do
   describe "GET #logout" do
     it "returns http success" do
       get :logout
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 
